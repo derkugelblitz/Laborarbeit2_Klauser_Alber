0cd94c42102a5b5aa4c8870bf9cd03e0b3295044
Sinnex03 <122963651+Sinnex03@users.noreply.github.com>
2023-06-14 19:25:50 +0000

    finishing the developer.cpp file
    added the ostream operator and the  senior developer and junior developer function to the developer.cpp file

--- code/src/developer.cpp
+++ code/src/developer.cpp
@@ -21,3 +21,30 @@
     auto Developer::get_dev_alias() const -> const std::string& {
         return dev_alias;
     }
+
+    auto operator<<(std::ostream& oss, const Developer& dev) -> const std__ostream&
+    {
+        return oss << "Name: " << dev.Developer::get_dev_name() << std::endl << "Alias: " << dev.Developer::get_dev_alias();
+
+    }
+
+    SeniorDeveloper::SeniorDeveloper(const std::string& name, const std::string& alias) : Developer(name, alias) {}
+
+    auto SeniorDeveloper::solve_problem() -> void
+    {
+        std::cout << "Solving a problem: " << std::endl;
+        std::cout << *this;std::cout << std::endl;
+        std::cout << "That was easy!" << std::endl;
+        Developer::drink_coffee();
+    }
+
+    JuniorDeveloper::JuniorDeveloper(const std::string& name, const std::string& alias) : Developer(name, alias) {}
+
+    auto JuniorDeveloper::solve_problem() -> void 
+{
+    
+    std::cout << "Solving a problem: " << std::endl;
+    std::cout << *this;std::cout << std::endl;
+    std::cout << "Puh that was though" << std::endl; 
+    Developer::drink_coffee();
+}
