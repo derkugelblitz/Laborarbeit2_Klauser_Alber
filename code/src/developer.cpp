4d30c711d691a51710ee3e4d65925de6cab5935f
derkugelblitz <122254574+derkugelblitz@users.noreply.github.com>
2023-06-16 07:18:10 +0000

    commenting main.cpp and developer.cpp

--- code/src/developer.cpp
+++ code/src/developer.cpp
@@ -3,33 +3,36 @@
 #include <string>
 
 
-    
+    // Implement constructor of base class
     Developer::Developer(const std::string& name, const std::string& alias) : dev_name(name), dev_alias(alias) {}
 
-    
+    // Implement drink coffee method
     auto Developer::drink_coffee()->void {
         std::cout << "Ahhhh, I needed that coffee!!!" << std::endl;
     }
 
-    
+    // Implement Getter for name
     auto Developer::get_dev_name() const -> const std::string&
     {
         return dev_name;
     }
 
-    
+    // Implement Getter for alias
     auto Developer::get_dev_alias() const -> const std::string& {
         return dev_alias;
     }
 
+    // Implement Stream operator to print Developer objects
     auto operator<<(std::ostream& oss, const Developer& dev) -> const std::ostream&
     {
         return oss << "Name: " << dev.Developer::get_dev_name() << std::endl << "Alias: " << dev.Developer::get_dev_alias();
 
     }
 
+    // Implement Constructor of Senior Developer
     SeniorDeveloper::SeniorDeveloper(const std::string& name, const std::string& alias) : Developer(name, alias) {}
 
+    // Implement solve problem method
     auto SeniorDeveloper::solve_problem() -> void
     {
         std::cout << "Solving a problem: " << std::endl;
@@ -38,8 +41,10 @@
         Developer::drink_coffee();
     }
 
+    // Implement Constructor of Junior Developer
     JuniorDeveloper::JuniorDeveloper(const std::string& name, const std::string& alias) : Developer(name, alias) {}
 
+    // Implement solve Problem method
     auto JuniorDeveloper::solve_problem() -> void 
 {
     
--- code/src/main.cpp
+++ code/src/main.cpp
@@ -6,13 +6,14 @@
 
 auto main()->int {
 
+    // Create a vector of shared pointer to Developer objects
     std::vector<std::shared_ptr<Developer>> developers;
 
-    
+    // Create developers and add to vector
     developers.push_back(std::make_shared<SeniorDeveloper>("Diana Prince", "Wonder Woman"));
     developers.push_back(std::make_shared<JuniorDeveloper>("Peter Parker", "Spiderman"));
 
-
+    // Use a loop and an iterator to go over the vector and let the developers solve a problem
     for (auto &it : developers)
      {
         it->solve_problem();
